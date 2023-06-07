a02f67c6dcea6328cbca40ca5173859810be1d1d
Javi H. Gil <javihgil@softspring.es>
2023-06-07 22:47:45 +0200

    Refactor dynamic form type to form extensions

--- tests/Form/Resolver/ConstraintResolverInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Softspring\Component\DynamicFormType\Form\Resolver;
-
-interface ConstraintResolverInterface
-{
-    public function resolveConstraintClass(string $constraint): string;
-}
--- tests/Form/Resolver/TypeResolverInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Softspring\Component\DynamicFormType\Form\Resolver;
-
-interface TypeResolverInterface
-{
-    public function resolveTypeClass(string $type): string;
-}
